1af47638767fe6cca0f912b7a9354338e09adb61
Andrey Ovsyannikov <bankid14@yandex.ru>
2020-08-30 21:01:42 +0300

    Initial commit

---
+++ FlightSearch/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  FlightSearch
+//
+//  Created by Андрей Овсянников on 30.08.2020.
+//  Copyright © 2020 home.com. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ FlightSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightSearch
+//
+//  Created by Андрей Овсянников on 30.08.2020.
+//  Copyright © 2020 home.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
