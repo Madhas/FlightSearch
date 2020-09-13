ccb67c93e0fb4412c7631fc0600c1d6d9cae7cd3
Andrey Ovsyannikov <bankid14@yandex.ru>
2020-09-13 15:31:24 +0300

    Add FlightSearch controller and presenter

---
+++ FlightSearch/FlightSearchController.swift
@@ -0,0 +1,14 @@
+//
+//  FlightSearchController.swift
+//  FlightSearch
+//
+//  Created by Андрей Овсянников on 13.09.2020.
+//  Copyright © 2020 home.com. All rights reserved.
+//
+
+import UIKit
+
+final class FlightSearchController: UIViewController {
+
+    
+}
