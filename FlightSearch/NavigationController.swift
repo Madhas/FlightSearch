b695f1a50512ac32182f307aa79610d474f74415
Andrey Ovsyannikov <bankid14@yandex.ru>
2020-09-13 17:30:29 +0300

    Add NavigationController

---
+++ FlightSearch/NavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationController.swift
+//  FlightSearch
+//
+//  Created by Андрей Овсянников on 13.09.2020.
+//  Copyright © 2020 home.com. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
