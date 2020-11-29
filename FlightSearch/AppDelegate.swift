202b6ad64af0f5d90a8dadf34cf1df5ff75c7a90
Andrey Ovsyannikov <bankid14@yandex.ru>
2020-11-29 22:09:03 +0300

    Assemble flights module in app delegate

--- FlightSearch/AppDelegate.swift
+++ FlightSearch/AppDelegate.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import HTTPWrapper
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let client = HTTPClient(configuration: .default)
+        let router = FlightSearchRouter(client: client)
+        
+        let window = UIWindow()
+        window.rootViewController = router.assembleModule()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
         return true
     }
 
