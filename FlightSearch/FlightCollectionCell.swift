7c9998fafaafbad8c8fcf16a858b80dca0d5d1c5
Andrey Ovsyannikov <bankid14@yandex.ru>
2020-11-29 22:06:32 +0300

    Update flight cell

--- FlightSearch/FlightCollectionCell.swift
+++ FlightSearch/FlightCollectionCell.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class FlightCollectionCell: UICollectionViewCell {
     
-    static private let titleHeight: CGFloat = 16
-    static private let subtitleHeight: CGFloat = 13
-    static private let interlabelSpace: CGFloat = 2
+    static private let titleHeight: CGFloat = 18
+    static private let subtitleHeight: CGFloat = 16
+    static private let interlabelSpace: CGFloat = 6
     
-    let titleLabel = UILabel()
-    let subtitleLabel = UILabel()
-    let iataLabel = UILabel()
+    var separatorEnabled = true {
+        didSet {
+            separator.isHidden = !separatorEnabled
+        }
+    }
+    
+    private let titleLabel = UILabel()
+    private let subtitleLabel = UILabel()
+    private let iataLabel = UILabel()
+    private let separator = UIView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,32 +34,47 @@ class FlightCollectionCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? .separator : .backgroundPrimary
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            backgroundColor = isSelected ? .separator : .backgroundPrimary
+        }
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         
         do {
             let size = iataLabel.sizeThatFits(CGSize(width: CGFloat.greatestFiniteMagnitude,
-                                                     height: 20))
+                                                     height: 22))
             let origin = CGPoint(x: bounds.width - 8 - size.width, y: bounds.midY - size.height / 2)
             iataLabel.frame = CGRect(origin: origin, size: size)
         }
         
-        let width: CGFloat = iataLabel.frame.minX - 8 * 2
+        let width: CGFloat = iataLabel.frame.minX - 16 - 8
         
-        let layoutHeight: CGFloat = FlightCollectionCell.titleHeight + (subtitleLabel.isHidden ? FlightCollectionCell.subtitleHeight + FlightCollectionCell.interlabelSpace : 0)
+        let layoutHeight: CGFloat = FlightCollectionCell.titleHeight + (subtitleLabel.isHidden ? 0 : FlightCollectionCell.subtitleHeight + FlightCollectionCell.interlabelSpace)
         let titleY = bounds.midY - layoutHeight / 2
         
-        titleLabel.frame = CGRect(x: 8,
+        titleLabel.frame = CGRect(x: 16,
                                   y: titleY,
                                   width: width,
                                   height: FlightCollectionCell.titleHeight)
         
         if !subtitleLabel.isHidden {
-            subtitleLabel.frame = CGRect(x: 8,
+            subtitleLabel.frame = CGRect(x: 16,
                                          y: titleLabel.frame.maxY + FlightCollectionCell.interlabelSpace,
                                          width: width,
                                          height: FlightCollectionCell.subtitleHeight)
         }
+        
+        let pixel = 1 / UIScreen.main.scale
+        separator.frame = CGRect(x: 16, y: bounds.height - pixel, width: bounds.width - 16, height: pixel)
     }
     
     func configure(with item: FlightCollectionItem) {
@@ -64,6 +86,23 @@ class FlightCollectionCell: UICollectionViewCell {
     }
     
     private func configureView() {
+        backgroundColor = .backgroundPrimary
         
+        titleLabel.textColor = .textPrimary
+        titleLabel.font = .systemFont(ofSize: 16)
+        addSubview(titleLabel)
+        
+        subtitleLabel.textColor = .textSecondary
+        subtitleLabel.font = .systemFont(ofSize: 14)
+        addSubview(subtitleLabel)
+        
+        iataLabel.textColor = .searchAccent
+        iataLabel.font = .systemFont(ofSize: 18, weight: .medium)
+        iataLabel.textAlignment = .center
+        addSubview(iataLabel)
+        
+        separator.backgroundColor = .separator
+        separator.isHidden = !separatorEnabled
+        addSubview(separator)
     }
 }
