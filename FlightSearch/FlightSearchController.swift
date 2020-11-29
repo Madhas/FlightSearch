71f04222b49cad21e8e643a4b04f8ecbd9e3508f
Andrey Ovsyannikov <bankid14@yandex.ru>
2020-11-29 23:24:38 +0300

    Implement controller and presenter

--- FlightSearch/FlightSearchController.swift
+++ FlightSearch/FlightSearchController.swift
@@ -8,7 +8,129 @@
 
 import UIKit
 
-final class FlightSearchController: UIViewController {
-
+protocol FlightSearchViewInput: AnyObject {
     
+    func show(items: [FlightCollectionItem])
+}
+
+final class FlightSearchController: UIViewController {
+    
+    var viewOutput: FlightSearchViewOutput!
+
+    private(set) var collectionView: UICollectionView!
+    private var dataSource: FlightsCollectionDataSource!
+    
+    private var currentKeyboardHeight: CGFloat = 0
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardChangeFrame(_:)),
+                                               name: UIResponder.keyboardWillChangeFrameNotification,
+                                               object: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureCollectionView()
+        configureSearchBar()
+    }
+    
+    private func configureCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .backgroundPrimary
+        collectionView.delegate = self
+        
+        dataSource = FlightsCollectionDataSource(collectionView: collectionView)
+        collectionView.dataSource = dataSource
+        
+        self.view.addSubview(collectionView)
+    }
+    
+    private func configureSearchBar() {
+        guard let navbarBounds = navigationController?.navigationBar.bounds else {
+            return
+        }
+        
+        let searchBar = SearchBar(frame: navbarBounds.insetBy(dx: 12, dy: 4))
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+    }
+    
+    @objc private func keyboardChangeFrame(_ notification: Notification) {
+        guard let frameValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber else {
+            return
+        }
+        
+        currentKeyboardHeight = frameValue.cgRectValue.origin.y < view.frame.maxY ? frameValue.cgRectValue.height : 0
+        if isViewLoaded {
+            UIView.animate(withDuration: animationDuration.doubleValue) {
+                self.collectionView.contentInset.bottom = self.currentKeyboardHeight
+            }
+        }
+    }
+}
+
+// MARK: FlightSearchViewInput
+
+extension FlightSearchController: FlightSearchViewInput {
+    
+    func show(items: [FlightCollectionItem]) {
+        dataSource.update(items: items)
+    }
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+
+extension FlightSearchController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return dataSource.itemSize(for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        let flight = dataSource.items[indexPath.item].flight
+        viewOutput.didSelect(flight: flight)
+    }
+}
+
+// MARK: SearchBarDelegate
+
+extension FlightSearchController: SearchBarDelegate {
+
+    func searchBarShouldBeginEditing(_ searchBar: SearchBar) -> Bool {
+        searchBar.showCancelButton(true, animated: true)
+        return true
+    }
+    
+    func searchBarCancelButtonTapped(_ searchBar: SearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        searchBar.showCancelButton(false, animated: true)
+        viewOutput.didEnter(query: "")
+    }
+    
+    func searchBarSearchButtonTapped(_ searchBar: SearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.showCancelButton(false, animated: true)
+    }
+    
+    func searchBar(_ searchBar: SearchBar, textDidChange text: String) {
+        viewOutput.didEnter(query: text)
+    }
 }
--- FlightSearch/FlightSearchPresenter.swift
+++ FlightSearch/FlightSearchPresenter.swift
@@ -8,12 +8,46 @@
 
 import Foundation
 
-final class FlightSearchPresenter {
+protocol FlightSearchViewOutput {
+    
+    func didEnter(query: String)
+    func didSelect(flight: Flight)
+}
 
+final class FlightSearchPresenter: FlightSearchViewOutput {
+
+    var moduleOutput: FlightSearchModuleOutput!
+    weak var viewInput: FlightSearchViewInput!
+    
     private let placesService: PlacesService
     
     init(placesService: PlacesService) {
         self.placesService = placesService
+        self.placesService.throttle = 0.5
     }
     
+    func didEnter(query: String) {
+        guard query.count > 0 else {
+            viewInput.show(items: [])
+            return
+        }
+        
+        placesService.loadPlaces(query: query) { result in
+            switch result {
+            case .success(let places):
+                let items = places.map {
+                    FlightCollectionItem(flight: $0)
+                }
+                DispatchQueue.main.async {
+                    self.viewInput.show(items: items)
+                }
+            case .failure(let error):
+                break
+            }
+        }
+    }
+    
+    func didSelect(flight: Flight) {
+        
+    }
 }
