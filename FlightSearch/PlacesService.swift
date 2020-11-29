449598edab5e23d882a10329d1f3cedf5bfe1af4
Andrey Ovsyannikov <bankid14@yandex.ru>
2020-11-29 22:08:49 +0300

    Add request throttling to PlacesService

--- FlightSearch/PlacesService.swift
+++ FlightSearch/PlacesService.swift
@@ -9,16 +9,37 @@
 import HTTPWrapper
 
 final class PlacesService {
+    
+    var throttle: TimeInterval = 0
 
     private let client: HTTPClient
+    
+    private var lastRequestData: (String, (Result<[Flight]>) -> Void)?
+    private var placesWorkItem: DispatchWorkItem?
     
     init(client: HTTPClient) {
         self.client = client
     }
     
     func loadPlaces(query: String, completion: @escaping (Result<[Flight]>) -> Void) {
-        let request = PlacesRequest(term: query)
-        client.send(request: request, completion: completion)
+        if placesWorkItem != nil {
+            lastRequestData = (query, completion)
+        } else if throttle > 0 {
+            lastRequestData = (query, completion)
+            let workItem = DispatchWorkItem(block: { [weak self] in
+                defer { self?.placesWorkItem = nil }
+                guard let (query, completion) = self?.lastRequestData else {
+                    return
+                }
+                let request = PlacesRequest(term: query)
+                self?.client.send(request: request, completion: completion)
+            })
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + throttle, execute: workItem)
+            placesWorkItem = workItem
+        } else {
+            let request = PlacesRequest(term: query)
+            client.send(request: request, completion: completion)
+        }
     }
 }
 
