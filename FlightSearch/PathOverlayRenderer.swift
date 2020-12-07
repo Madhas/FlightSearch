788522391b106e6fbb99c5538aa7fb193a37d029
Andrey Ovsyannikov <bankid14@yandex.ru>
2020-12-07 17:47:41 +0300

    Use calculator in PathOverlayRenderer

--- FlightSearch/PathOverlayRenderer.swift
+++ FlightSearch/PathOverlayRenderer.swift
@@ -9,8 +9,12 @@
 import MapKit
 
 final class PathOverlayRenderer: MKOverlayPathRenderer {
+
+    private let calculator: FlightPathCalculator
     
     override init(overlay: MKOverlay) {
+        calculator = FlightPathCalculator()
+        
         super.init(overlay: overlay)
         
         strokeColor = .accent
@@ -21,31 +25,12 @@ final class PathOverlayRenderer: MKOverlayPathRenderer {
 
     override func createPath() {
         guard let overlay = overlay as? PathOverlay else { return }
-
+        
         let rect = self.rect(for: overlay.boundingMapRect)
         let startPoint = self.point(for: overlay.startPoint)
         let endPoint = self.point(for: overlay.endPoint)
-        
-        let amp = max(min(rect.height, rect.width), max(rect.height, rect.width) / 4)
-        let ctrlPoint1: CGPoint
-        let ctrlPoint2: CGPoint
-        
-        if rect.width > rect.height {
-            ctrlPoint1 = CGPoint(x: abs(startPoint.x - rect.width / 3),
-                                 y: rect.midY + amp)
-            ctrlPoint2 = CGPoint(x: abs(startPoint.x - rect.width * 2 / 3),
-                                 y: rect.midY - amp)
-        } else {
-            ctrlPoint1 = CGPoint(x: rect.midX + amp,
-                                 y: abs(startPoint.y - rect.height / 3))
-            ctrlPoint2 = CGPoint(x: rect.midX - amp,
-                                 y: abs(startPoint.y - rect.height * 2 / 3))
-        }
-        
-        let path = UIBezierPath()
-        path.move(to: startPoint)
-        path.addCurve(to: endPoint, controlPoint1: ctrlPoint1, controlPoint2: ctrlPoint2)
-        
-        self.path = path.cgPath
+
+        calculator.update(boundingRect: rect, start: startPoint, end: endPoint)
+        self.path = calculator.cubicCurve().cgPath
     }
 }
