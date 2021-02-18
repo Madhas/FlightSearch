ac7b95af20cb49e701e86050c5efcd9d96443427
Andrey Ovsyannikov <bankid14@yandex.ru>
2021-02-18 22:54:23 +0300

    Inherit FlightsCollectionDataSource from CollectionDataSource

--- FlightSearch/FlightsCollectionDataSource.swift
+++ FlightSearch/FlightsCollectionDataSource.swift
@@ -1,42 +1,12 @@
 import UIKit
 
-class FlightsCollectionDataSource: NSObject {
-
-    private(set) var items = [FlightCollectionItem]()
-    
-    private weak var collectionView: UICollectionView!
-    
-    init(collectionView: UICollectionView) {
-        self.collectionView = collectionView
-        
-        super.init()
-        
-        collectionView.register(cellClass: FlightCollectionCell.self)
-        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            layout.minimumLineSpacing = .leastNonzeroMagnitude
-        }
-    }
+final class FlightsCollectionDataSource: CollectionDataSource {
     
     func update(items: [FlightCollectionItem]) {
-        self.items = items
-        collectionView.reloadData()
-    }
-    
-    func itemSize(for indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.bounds.width, height: 70)
-    }
-}
-
-extension FlightsCollectionDataSource: UICollectionViewDataSource {
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeue(FlightCollectionCell.self, for: indexPath)
-        cell.configure(with: items[indexPath.item])
-        cell.separatorEnabled = indexPath.item < items.count - 1
-        return cell
+        let section = CollectionSectionDescriptor(items: items) { (cell: FlightCollectionCell, item, indexPath) in
+            cell.configure(with: item)
+            cell.separatorEnabled = indexPath.item < items.count - 1
+        }
+        sections = [section]
     }
 }
