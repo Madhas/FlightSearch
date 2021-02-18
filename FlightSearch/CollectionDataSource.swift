3389caafd9947d2817c7797a3eb2a08c612471b3
Andrey Ovsyannikov <bankid14@yandex.ru>
2021-02-18 22:54:06 +0300

    Add CollectionDataSource

---
+++ FlightSearch/CollectionDataSource.swift
@@ -0,0 +1,42 @@
+//
+//  CollectionDataSource.swift
+//  FlightSearch
+//
+//  Created by Андрей Овсянников on 18.02.2021.
+//  Copyright © 2021 home.com. All rights reserved.
+//
+
+import UIKit
+
+class CollectionDataSource: NSObject {
+
+    var sections: [CollectionSectionDescriptor] = []
+    
+    private var registeredIdentifiers: Set<String> = []
+    
+    func clear() {
+        sections = []
+    }
+}
+
+extension CollectionDataSource: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sections[section].items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let section = sections[indexPath.section]
+        if !registeredIdentifiers.contains(section.cellsReusableIdentifier) {
+            collectionView.register(section.cellsClass, forCellWithReuseIdentifier: section.cellsReusableIdentifier)
+        }
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: section.cellsReusableIdentifier, for: indexPath)
+        section.configure(cell, indexPath)
+        return cell
+    }
+}
