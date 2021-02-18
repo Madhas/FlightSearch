82976051cc6a9227fb2f9d7e62532e70cbb81458
Andrey Ovsyannikov <bankid14@yandex.ru>
2021-02-18 22:40:27 +0300

    Add CollectionSectionDescriptor

---
+++ FlightSearch/CollectionSectionDescriptor.swift
@@ -0,0 +1,26 @@
+//
+//  CollectionSectionDescriptor.swift
+//  FlightSearch
+//
+//  Created by Андрей Овсянников on 18.02.2021.
+//  Copyright © 2021 home.com. All rights reserved.
+//
+
+import UIKit
+
+final class CollectionSectionDescriptor {
+
+    let cellsReusableIdentifier: String
+    let items: [Any]
+    let configure: (UICollectionViewCell, IndexPath) -> Void
+    
+    init<Cell: UICollectionViewCell, Item>(items: [Item],
+                                           configure: @escaping (Cell, Item, IndexPath) -> Void) {
+        
+        cellsReusableIdentifier = String(describing: Cell.self)
+        self.items = items
+        self.configure = { cell, indexPath in
+            configure(cell as! Cell, items[indexPath.item], indexPath)
+        }
+    }
+}
