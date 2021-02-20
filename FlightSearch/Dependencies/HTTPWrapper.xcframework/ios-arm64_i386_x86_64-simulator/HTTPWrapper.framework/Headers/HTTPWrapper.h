83c9a6b5f7b05f29fb5f32bb1972c3e3e54b56bc
Andrey Ovsyanikov <bankid14@yandex.ru>
2021-02-20 22:06:38 +0300

    Put xcframeworks in project directory

---
+++ FlightSearch/Dependencies/HTTPWrapper.xcframework/ios-arm64_i386_x86_64-simulator/HTTPWrapper.framework/Headers/HTTPWrapper.h
@@ -0,0 +1,19 @@
+//
+//  HTTPWrapper.h
+//  HTTPWrapper
+//
+//  Created by Andrey Ovsyannikov on 11/08/2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTPWrapper.
+FOUNDATION_EXPORT double HTTPWrapperVersionNumber;
+
+//! Project version string for HTTPWrapper.
+FOUNDATION_EXPORT const unsigned char HTTPWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPWrapper/PublicHeader.h>
+
+
