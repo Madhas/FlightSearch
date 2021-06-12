7ba09a53441c88eb90aa5f97bd6b018766c71c22
Andrey Ovsyanikov <bankid14@yandex.ru>
2021-06-12 19:48:51 +0300

    Remove animation logic from annotation and annotation view

--- FlightSearch/PlaneAnnotation.swift
+++ FlightSearch/PlaneAnnotation.swift
@@ -11,25 +11,10 @@ import MapKit
 final class PlaneAnnotation: NSObject, MKAnnotation {
 
     @objc dynamic var coordinate: CLLocationCoordinate2D
-    var startTime: Double = CACurrentMediaTime()
-    var directionForward = true
     
-    let startPoint: MKMapPoint
-    let endPoint: MKMapPoint
-    let boundingRect: MKMapRect
-    
-    let camera: MKMapCamera
-    
-    init(start: Flight.Location, finish: Flight.Location, camera: MKMapCamera) {
-        let startCoordinate = CLLocationCoordinate2D(latitude: start.latitude, longitude: start.longitude)
-        let finishCoordinate = CLLocationCoordinate2D(latitude: finish.latitude, longitude: finish.longitude)
-        
-        coordinate = startCoordinate
-        startPoint = MKMapPoint(startCoordinate)
-        endPoint = MKMapPoint(finishCoordinate)
-        boundingRect = MKMapRect(start: start, finish: finish)
-        
-        self.camera = camera
+    init(coordinate: Flight.Location) {
+        let startCoordinate = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        self.coordinate = startCoordinate
         
         super.init()
     }
--- FlightSearch/PlaneAnnotationView.swift
+++ FlightSearch/PlaneAnnotationView.swift
@@ -9,11 +9,6 @@
 import MapKit
 
 final class PlaneAnnotationView: MKAnnotationView {
-    
-    static private let animationDuration: Double = 7
-    
-    private let calculator = FlightPathCalculator()
-    private var displayLink: CADisplayLink?
 
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
@@ -21,49 +16,9 @@ final class PlaneAnnotationView: MKAnnotationView {
         image = .plane
         frame.size = CGSize(width: 35, height: 35)
         isEnabled = false
-
-        if let annotation = annotation as? PlaneAnnotation {
-            calculator.update(boundingRect: Rect(rect: annotation.boundingRect),
-                              start: Point(point: annotation.startPoint),
-                              end: Point(point: annotation.endPoint))
-            
-        }
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func prepareForDisplay() {
-        super.prepareForDisplay()
-        if displayLink == nil {
-            displayLink = CADisplayLink(target: self, selector: #selector(tick(_:)))
-            displayLink?.add(to: .main, forMode: .common)
-        }
-    }
-    
-    @objc private func tick(_ displayLink: CADisplayLink) {
-        guard let annotation = annotation as? PlaneAnnotation else { return }
-        
-        let passed = CACurrentMediaTime() - annotation.startTime
-        let progress: Double
-        if annotation.directionForward {
-            progress = passed / PlaneAnnotationView.animationDuration
-        } else {
-            progress = 1 - passed / PlaneAnnotationView.animationDuration
-        }
-        
-        guard progress <= 1, progress >= 0 else {
-            annotation.startTime = CACurrentMediaTime()
-            annotation.directionForward.toggle()
-            return
-        }
-        
-        let center = calculator.point(from: progress)
-        let angle = calculator.angle(from: progress) - (annotation.camera.heading * .pi / 180)
-        
-        annotation.coordinate = center.mapPoint.coordinate
-        
-        transform = CGAffineTransform(rotationAngle: CGFloat(annotation.directionForward ? angle : angle + .pi))
     }
 }
