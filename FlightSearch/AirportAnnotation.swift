53cdbd1dc8ad9d769410bc9304ccab4a40510925
Andrey Ovsyanikov <bankid14@yandex.ru>
2021-06-12 19:48:18 +0300

    Move airport annotation drawing into Annotation class to save images between reuses

--- FlightSearch/AirportAnnotation.swift
+++ FlightSearch/AirportAnnotation.swift
@@ -11,10 +11,33 @@ import MapKit
 final class AirportAnnotation: NSObject, MKAnnotation {
     
     let coordinate: CLLocationCoordinate2D
-    let title: String?
+    private let iata: String
+    
+    private(set) lazy var image = drawImage()
     
     init(coordinate: CLLocationCoordinate2D, title: String) {
         self.coordinate = coordinate
-        self.title = title
+        iata = title
+    }
+
+    func drawImage() -> UIImage {
+        let view = UIView(frame: CGRect(origin: .zero, size: AirportAnnotationView.imageSize))
+        view.backgroundColor = .accent
+        view.layer.cornerRadius = 6
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.white.cgColor
+        
+        let label = UILabel()
+        label.text = iata
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 12)
+        label.sizeToFit()
+        label.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        
+        view.addSubview(label)
+        
+        return UIGraphicsImageRenderer(size: AirportAnnotationView.imageSize).image { ctx in
+            view.layer.render(in: ctx.cgContext)
+        }
     }
 }
--- FlightSearch/AirportAnnotationView.swift
+++ FlightSearch/AirportAnnotationView.swift
@@ -11,38 +11,19 @@ import MapKit
 final class AirportAnnotationView: MKAnnotationView {
     
     static let imageSize = CGSize(width: 36, height: 20)
+    
+    override var annotation: MKAnnotation? {
+        didSet {
+            guard let annotation = self.annotation as? AirportAnnotation else { return }
+            image = annotation.image
+        }
+    }
 
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
-        
-        isEnabled = false
-        drawView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func drawView() {
-        guard let ttitle = annotation?.title, let title = ttitle else { return }
-        
-        let view = UIView(frame: CGRect(origin: .zero, size: AirportAnnotationView.imageSize))
-        view.backgroundColor = .accent
-        view.layer.cornerRadius = 6
-        view.layer.borderWidth = 1
-        view.layer.borderColor = UIColor.white.cgColor
-        
-        let label = UILabel()
-        label.text = title
-        label.textColor = .white
-        label.font = .systemFont(ofSize: 12)
-        label.sizeToFit()
-        label.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
-        
-        view.addSubview(label)
-        
-        image = UIGraphicsImageRenderer(size: AirportAnnotationView.imageSize).image { ctx in
-            view.layer.render(in: ctx.cgContext)
-        }
     }
 }
